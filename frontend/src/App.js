3a7e203ef3a615deb9999d6ce9437327f88e22dd
Senfire <eric.mcghee@northwestern.edu>
2023-03-15 16:22:22 -0500

    Google login component for testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from 'react-google-login'
 
 function App() {
+
+  
+
   return (
+    <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Calendar API</h1>
+    </div>
+    <div>
+      <GoogleLogin clientId='807282470908-stjl2ss7bkr9plo7kdn3ranhnfo61gcg.apps.googleusercontent.com'
+      buttonText='Sign in & Authorize Calendar' 
+      onSuccess={responseGoogle}
+      onFailure={responseError}
+      cookiePolicy={'single_host_origin'}
+      //important - this step is for receiving a refresh token on response
+      responseType= 'code'
+      accessType= 'offline'
+      scope= 'openId email profile https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events'
+      />
+    </div>
     </div>
   );
 }
